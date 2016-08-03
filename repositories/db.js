7327f88f02b4ca5d28fa742c0498d22789f74769
Zumium <martin007323@gmail.com>
2016-08-03 15:13:53 +0800

    finish user model

---
+++ repositories/db.js
@@ -0,0 +1,25 @@
+var mongoose=require('mongoose');
+
+mongoose.connect('mongodb://mongodb/locationpost');
+
+var personSchema=mongoose.Schema({
+	name:String,
+	portrait:String,
+	age:{type:Number,min:0,max:200},
+	gender:String,
+	birthday:Date,
+	briefIntro:String
+},{id:false,_id:false});
+
+var accountSchema=mongoose.Schema({
+	username:String,
+	password:String
+},{id:false,_id:false});
+
+var userSchema=mongoose.Schema({
+	person:personSchema,
+	account:accountSchema,
+	follow:[Schema.Types.ObjectId]
+});
+
+exports.User=mongoose.model('User',userSchema);
