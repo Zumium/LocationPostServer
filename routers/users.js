ecfc366a2018e9acfcf4704ab427e5420b463e15
Zumium <martin007323@gmail.com>
2016-08-09 17:43:32 +0800

    add authentication at PATCH /users/:username

--- routers/users.js
+++ routers/users.js
@@ -2,6 +2,7 @@ var us=require('../services/userservice');
 var genError=require('../tools/gene-error');
 var userTools=require('../tools/user');
 var express=require('express');
+var passport=require('passport');
 
 var router=module.exports=express.Router();
 
@@ -22,16 +23,19 @@ router.get('/:username',(req,res,next)=>{
 		.catch(next);
 });
 
-router.patch('/:username',(req,res,next)=>{
-	//permission check
-	if(req.user!=req.params.username) throw genError(403,'Not permitted');
-	us.findOneByUsername(req.params.username)
-		.then((user)=>{
-			return user.update(req.body);
-		})
-		.then((result)=>{
-			if(result.ok==0) throw genError(400,'Operation failed due to wrong request');
-			res.sendStatus(200);
-		})
-		.catch(next);
-});
+router.patch('/:username',
+		passport.authenticate('basic',{session:false}),
+		(req,res,next)=>{
+			//permission check
+			if(req.user!=req.params.username) throw genError(403,'Not permitted');
+			us.findOneByUsername(req.params.username)
+				.then((user)=>{
+					return user.update(req.body);
+				})
+				.then((result)=>{
+					if(result.ok==0) throw genError(400,'Operation failed due to wrong request');
+					res.sendStatus(200);
+				})
+				.catch(next);
+		}
+);
